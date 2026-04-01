fd5231a5b93a80de532155a707d3a4a6068fe129
Kirill Pinchuk <cybergrind@gmail.com>
2026-04-02 00:32:03 +0300

    x64: get_stack_pointer

--- src/bin/stackinspector.rs
+++ src/bin/stackinspector.rs
@@ -6,6 +6,12 @@ unsafe extern "C" fn get_stack_pointer() -> *const u8 {
     naked_asm!("mov x0, sp", "ret");
 }
 
+#[cfg(target_arch = "x86_64")]
+#[unsafe(naked)]
+unsafe extern "C" fn get_stack_pointer() -> *const u8 {
+    naked_asm!("mov rax, rsp", "ret");
+}
+
 fn main() {
     let stack_pointer: *const u8;
     unsafe {
